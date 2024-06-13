3fef8ca8ebba92ecb8cb87bca3358eb93d45c0c6
Nayan Kumar <91410666+Nayankumar4986@users.noreply.github.com>
2024-06-13 21:29:03 +0530

    Add files via upload

---
+++ Windows Forms App/Windows Forms App/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+    End Sub
+
+    ' Event handler for the button click event
+    Private Sub ButtonClickHandler(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Your Name here, this is my Slidely AI task")
+    End Sub
+End Class
